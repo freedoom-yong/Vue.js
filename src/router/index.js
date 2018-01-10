04f965f13a4b384cd602895b7ef07484f5a45476
freedoom-yong <648683283@qq.com>
2018-01-10 15:49:24 +0800

    新增路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Demo from '../demo/Demo'
+import demo1 from '../demo/demo1'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/demo',
+      name: 'Demo',
+      component: Demo
+    },
+    {
+      path: '/demo1',
+      name: 'demo1',
+      component: demo1
     }
   ]
 })
