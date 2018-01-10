d66521e7e5a7e53d4b86b1b634bae119dd1e93f8
freedoom-yong <648683283@qq.com>
2018-01-10 15:51:33 +0800

    新增路由

---
+++ src/demo/demo1.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id= "demo1">
+    <div v-html= 'msg'></div>
+  </div>
+</template>
+
+<script>
+  export default({
+    name: 'demo1',
+    data () {
+      return {
+        msg: '<h1>你好！</h1>'
+      }
+    }
+  })
+</script>
