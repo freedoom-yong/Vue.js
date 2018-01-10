a9a03be35a30c0104b956d786c341b5e68039bd4
freedoom-yong <648683283@qq.com>
2018-01-10 15:39:15 +0800

    vue 文本插值

---
+++ src/demo/Demo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id= demo>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'demo',
+    data () {
+      return {
+        message: 'hello vue.js！'
+      }
+    }
+  }
+</script>
