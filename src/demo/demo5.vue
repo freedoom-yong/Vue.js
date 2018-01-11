d80bbafcf1e1eec1930e7bef88168078d697d14e
freedoom-yong <648683283@qq.com>
2018-01-11 18:01:36 +0800

    v-on指令绑定click事件

---
+++ src/demo/demo5.vue
@@ -0,0 +1,24 @@
+//v-on指令绑定click事件，反转字符串
+<template>
+  <div id="demo5">
+    <p>{{ message }}</p>
+    <button v-on:click="reverseMessage">反转字符串</button>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'demo5',
+    data () {
+      return {
+        message: 'you are very good!'
+      }
+    },
+    methods: {
+      reverseMessage: function () {
+        alert('nihao!' + this.message)
+        this.message = this.message.split('').reverse().join('')
+      }
+    }
+  }
+</script>
