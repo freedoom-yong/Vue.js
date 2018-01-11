c732cd55a3209be79cff3b0608d12ef3c9ffdea2
freedoom-yong <648683283@qq.com>
2018-01-11 18:01:02 +0800

    v-if隐藏字段

---
+++ src/demo/demo4.vue
@@ -0,0 +1,28 @@
+//v-if隐藏字段
+<template>
+  <div id= "demo4">
+    <div v-if="seen">
+      <h1 v-if="one">我是最大的</h1>
+      <h2 v-if="two">我是第二大</h2>
+      <h3 v-if="three">我是第三大</h3>
+      <h4 v-if="four">我是第四大</h4>
+      <h5 v-if="five">我最小</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default({
+    name: 'demo4',
+    data () {
+      return {
+        seen: true,
+        one: true,
+        two: true,
+        three: false,
+        four: true,
+        five: true
+      }
+    }
+  })
+</script>
