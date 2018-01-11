9023aa83bbdfc3ad933046cb0ff35b98e73460ea
freedoom-yong <648683283@qq.com>
2018-01-11 18:01:56 +0800

    过滤器应用

---
+++ src/demo/demo6.vue
@@ -0,0 +1,25 @@
+//过滤器应用
+<template>
+  <div id="demo6">
+    {{ message | capitalize }}<br/>
+    <input v-model="message">
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'demo6',
+    data () {
+      return {
+        message: 'hello'
+      }
+    },
+    filters: {
+      capitalize: function (value) {
+        if (!value) return ''
+        value = value.toString()
+        return value.charAt(0).toUpperCase() + value.slice(1)
+      }
+    }
+  }
+</script>
