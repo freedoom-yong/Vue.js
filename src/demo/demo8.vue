15b921d430caf6ae536a407ee3aca14a2dbbc223
freedoom-yong <648683283@qq.com>
2018-01-13 15:30:56 +0800

    v-for应用

---
+++ src/demo/demo8.vue
@@ -0,0 +1,31 @@
+<--! v-for 应用 -->
+<template>
+  <div id="demo8">
+    <ul>
+      <li v-for="(value,key,index) in object">{{ index }}. {{key}} : {{ value }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  document.write(Date())
+  export default{
+    name: 'demo8',
+    data () {
+      return {
+        object: {
+          name: 'anple',
+          url: 'www.emmm.com',
+          solgan: 'banana'
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  div{
+    text-align: left;
+    font: italic bold 20px arial,serif;
+  }
+</style>
