067c620858e470111015c35ea060b6ddb81d7219
freedoom-yong <648683283@qq.com>
2018-01-13 15:29:05 +0800

    v-else-if应用

---
+++ src/demo/demo7.vue
@@ -0,0 +1,25 @@
+<--! v-else-if的应用 -->
+<template>
+  <div id="demo7">
+    <div v-if="num > 5">
+      {{ num }}大于5
+    </div>
+    <div v-else-if="num == 5">
+      {{ num }}等于5
+    </div>
+    <div v-else>
+      {{ num }}小于5
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'demo7',
+    data () {
+      return {
+        num: Math.floor(10 * Math.random() + 1)
+      }
+    }
+  }
+</script>
