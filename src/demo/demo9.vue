e55b70cbb16c67e0b0d3dff02acc73f5b5e5c406
freedoom-yong <648683283@qq.com>
2018-01-17 17:29:50 +0800

    计算后反转数据及过滤器应用

---
+++ src/demo/demo9.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="demo9">
+    <p>原始数据：{{ message }}</p>
+    <p>计算后反转的数据：{{ reversedMessage | capitalize }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'demo9',
+    data () {
+      return {
+        message: 'Vue.js'
+      }
+    },
+    computed: {
+      reversedMessage: function () {
+        return this.message.split('').reverse().join('')
+      }
+    },
+    filters: {
+      capitalize: function (value) {
+        if (!value) return ''
+        value = value.toString()
+        return value.charAt(0).toUpperCase() + value.slice(1)
+      }
+    }
+  }
+</script>
