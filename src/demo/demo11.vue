0bafaa06e6390eb55360dac25fe11dc343285d3e
freedoom-yong <648683283@qq.com>
2018-01-22 15:36:14 +0800

    绑定class元素应用

---
+++ src/demo/demo11.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="demo11">
+    <div v-bind:class="{ active: isActive}"></div>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'demo11',
+    data () {
+      return {
+        isActive: true
+      }
+    }
+  }
+</script>
+
+<style>
+  .active {
+    width: 100px;
+    height: 100px;
+    background: #8e8aff;
+    margin: auto;
+  }
+</style>
